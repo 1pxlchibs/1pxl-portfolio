a01ca504403b39a1d1f419aa3666ffe181a79b4c
1pxlchibs <therealchibouki@gmail.com>
2024-02-09 18:32:41 -0500

    another dist push!

---
+++ commands/hello.ts
@@ -0,0 +1,42 @@
+const helloObj = {
+  "message" : [
+    [
+      "Hello visitor!",
+    ],
+    [
+      "Hey!",
+    ],
+    [
+      "Hello World!",
+    ],
+    [
+      "01001000 01000101 01001100",
+    ],
+    [
+      "Good day to you!",
+    ],
+    [
+      "Bonjour!",
+    ],
+    [
+      "Hola!",
+    ],
+    [
+      "Heya!",
+    ],
+    [
+      "Greetings human!",
+    ],
+  ],
+}
+
+export const createHello = () : string[] => {
+  const hello : string[] = [];  
+  const r = Math.floor(Math.random() * helloObj.message.length);
+  
+  helloObj.message[r].forEach((ele) => {
+    hello.push(ele);
+  });
+
+  return hello
+}
